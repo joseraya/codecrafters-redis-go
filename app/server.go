91b31c1cfe07daa82e5c22c2eebda2a49eb86cea
Jose Raya <jose.raya@agilogy.com>
2023-03-17 21:12:04 +0100

    PONG

--- app/server.go
+++ app/server.go
@@ -6,6 +6,15 @@ import (
 	"os"
 )
 
+func writeString(conn net.Conn, message string) (int, error) {
+	msg := fmt.Sprintf("+%s\r\n", message)
+	n, err := conn.Write([]byte(msg))
+	if err != nil {
+		fmt.Println("Error writing the response", err.Error())
+		return 0, err
+	}
+	return n, nil
+}
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -14,9 +23,20 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+
+	buf := make([]byte, 0, 4096)
+	_, err = conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading from the connection", err.Error())
+	}
+	_, writeErr := writeString(conn, "PONG")
+
+	if writeErr != nil {
+		fmt.Println("Write error", writeErr)
 	}
 }
