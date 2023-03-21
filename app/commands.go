bc6b89a87dbcaefd74b39b38d35f82ba744fd1a7
Jose Raya <jose.raya@agilogy.com>
2023-03-21 20:01:56 +0100

    set and get commands

--- app/commands.go
+++ app/commands.go
@@ -4,6 +4,14 @@ import (
 	"strings"
 )
 
+// This is not thread safe. Probably would break in a real-life
+// scenario. We should probably be using https://pkg.go.dev/sync#Map instead
+var values map[string]string
+
+func init() {
+	values = map[string]string{}
+}
+
 func HandleCommand(request []string) []string {
 	if len(request) == 0 {
 		return nil
@@ -16,6 +24,10 @@ func HandleCommand(request []string) []string {
 		return echo(request[1:])
 	case "COMMAND":
 		return command(request[1:])
+	case "SET":
+		return set(request[1:])
+	case "GET":
+		return get(request[1:])
 	default:
 		return nil
 	}
@@ -31,4 +43,17 @@ func echo(args []string) []string {
 
 func command(args []string) []string {
 	return []string{"DOCS"}
+}
+
+func set(args []string) []string {
+	key := args[0]
+	value := args[1]
+	values[key] = value
+	return []string{"OK"}
+}
+
+func get(args []string) []string {
+	key := args[0]
+	value := values[key]
+	return []string{value}
 }
